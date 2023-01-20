798e77cbad4d2517acf690bfa625501f8b215967
Minoo7 <accounts@minoo.pro>
2023-01-20 15:25:54 +0100

    lab0 finished, but valgrind not checked

--- pintos-linked-list/main.c
+++ pintos-linked-list/main.c
@@ -7,18 +7,67 @@ struct student {
 	char *name;
 	struct list_elem elem;
 };
+typedef struct student student;
+typedef struct list_elem list_elem;
 
 
+// Vrf funkar det med malloc och utan? : malloc(sizeof(student)); : (char*) malloc(sizeof(char));
 void insert (struct list *student_list) {
+	student *new_student = malloc(sizeof(student));
+	char *name = (char*) malloc(sizeof(char));
+
+	// fetch name
+	printf("What's your name: ");
+	scanf("%s", name);
+	new_student->name = name;
+	
+	// Puts the student last in the list (next to tail)
+	list_push_back(student_list, &new_student->elem);	
 }
 
 void delete (struct list *student_list) {
+	// Fetches a name
+	char *name = (char*) malloc(sizeof(char));
+	printf("Enter a name to delete: \n");
+	scanf("%s", name);
+
+	// Loop until we find a matching name 
+	for (list_elem *e = list_begin (student_list); e != list_end (student_list); e = list_next (e)) {
+		student *stud = list_entry(e, struct student, elem);
+		if (!strcmp(stud->name, name)) {
+			list_remove(&stud->elem);
+			free(stud->name);
+			free(stud);
+			free(name);
+			return;
+		}
+	}
+	printf("No student with that name exists\n");
 }
 
-void list (struct list *student_list) {
+void print (struct list *student_list) {
+	list_elem *e;
+	if (list_empty(student_list)) {
+		printf("There are no students in this class!\n");
+	}
+	else {
+		for (e = list_begin (student_list); e != list_end (student_list); e = list_next (e))
+			{
+				student *stud = list_entry(e, struct student, elem);
+				printf("Name is: %s\n", stud->name);
+			}
+	}
 }
 
-void quit (struct list *student_list) {
+void clear (struct list *student_list) {
+	// Loop through list and pop element from the front 
+	while (!list_empty (student_list)) {
+		list_elem *e = list_pop_front(student_list);
+		student *stud = list_entry(e, struct student, elem);
+		free(stud->name);
+		free(stud);
+		// free(e);
+	}
 }
 
 int main() {
@@ -30,8 +79,8 @@ int main() {
 		printf("Menu:\n");
 		printf("1 - Insert student\n");
 		printf("2 - Delete student\n");
-		printf("3 - List students\n");
-		printf("4 - Exit\n");
+		printf("3 - Print students\n");
+		printf("4 - Clear\n");
 		scanf("%d", &opt);
 		switch (opt) {
 			case 1:
@@ -46,13 +95,13 @@ int main() {
 				}
 			case 3:
 				{
-					list(&student_list);
+					print(&student_list);
 					break;
 				}
 
 			case 4:
 				{
-					quit(&student_list);
+					clear(&student_list);
 					break;
 				}
 			default:
@@ -60,7 +109,7 @@ int main() {
 					printf("Quit? (1/0):\n");
 					scanf("%d", &opt);
 					if (opt)
-						quit(&student_list);
+						clear(&student_list);
 					break;
 				}
 		}
